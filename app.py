a53a35d6d20e7c7dbda43b6499eec501593428ca
k-kovacik <kkovacik91@gmail.com>
2025-07-30 08:56:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ with st.sidebar:
   bill_depth_mm = st.slider('Bill Depth (mm)', 13.0,22.0,17.5)
   flipper_length = st.slider('Flipper Length (mm)', 172.0,231.0,201.0)
   body_mass = st.slider('Body Mass (g)', 2500,6500,4500)
-  
+  gender = st.selectbox('Gender', ['Male', 'Female'])
   
 
