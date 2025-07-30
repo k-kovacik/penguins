9dc762768091ad0425e6245235b3ae270b40b3ce
k-kovacik <kkovacik91@gmail.com>
2025-07-30 08:25:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from scikit.learn import RandomForestClassifier
 
 st.title("Penguin Species Prediction ML app")
