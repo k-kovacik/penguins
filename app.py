e33cc926389130d73edf91bdcb86edb733c7eb8f
k-kovacik <kkovacik91@gmail.com>
2025-07-30 08:35:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,19 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 st.title("Penguin Species Prediction ML app")
+st.info("This is an end-to-end Machine Learning App")
+
+with st.expander("Data"):
+  pass
+
+with st.expander("Data Visualization"):
+  pass
+  
+with st.expander("Input Data"):
+  pass
+
+with st.expander("Data Preparation"):
+  pass
+
+with st.sidebar:
+  pass
